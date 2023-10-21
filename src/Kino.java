839cb5e3e6056b48af35afb67e59b483d44243c0
bszarlowicz <b,szarlowicz@gmail.com>
2023-10-21 11:44:15 +0200

    Seans and seats settings

--- src/Kino.java
+++ src/Kino.java
@@ -3,9 +3,15 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Kino implements Serializable {
-    private String nazwa;
-    private String adres;
-    private ArrayList<Seans> seanse;
-    private ArrayList<Pracownik> pracownicy;
-    private ArrayList<Bilet> bilety;
+    private String name;
+    private String address;
+    private ArrayList<Seans> seans;
+    private ArrayList<Pracownik> employees;
+    //private ArrayList<Bilet> tickets;
+
+    public Kino(String name, String address, ArrayList<Seans> seans) {
+        this.name = name;
+        this.address = address;
+        this.seans = seans;
+    }
 }
